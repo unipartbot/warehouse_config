9d3f04a7854812a4e7f5342e1e7ab70b3653c03c
nocturnalastro <thenocturnalastrostudent@gmail.com>
2018-02-08 11:39:56 +0000

    [IMP] refactored test so that user, company and warhouse are defined in setup

--- tests/test_res_users.py
+++ tests/test_res_users.py
@@ -1,88 +1,55 @@
 # -*- coding: utf-8 -*-
 
-from odoo.tests import common
 from odoo.exceptions import ValidationError
+from .common import BaseWarehouseConfig
 
-class BaseBlocked(common.SavepointCase):
+class TestGetUserWarehouse(BaseWarehouseConfig):
 
-    def test_01_get_user_warehouse_no_user(self):
+    def test01_get_user_warehouse_no_user(self):
         """Checking that when a user no user is found
            the correct error is raised
         """
-        User = self.env['res.users']
-        # Creating user without company so takes company from current user
-        test_user = User.create({'name': 'test_user',
-                                 'login': '12345678910',
-                                 'active': False})
-
+        # Make it so user isn't found in search
+        self.test_user.active = False
         with self.assertRaises(ValidationError) as e:
-            User.sudo(test_user).get_user_warehouse()
+             self.do_as_user(self.test_user).get_user_warehouse()
 
         self.assertEqual(e.exception.name, 'Cannot find user to get warehouse.')
 
-    def test_02_get_user_warehouse_no_warehouse(self):
+    def test02_get_user_warehouse_no_warehouse(self):
         """Checking that when a user has no warehouse 
            the correct error is raised
         """
-        User = self.env['res.users']
-        Warehouse = self.env['stock.warehouse']
-
-        # Creating user without company so takes company from current user
-        test_user = User.create({'name': 'test_user',
-                                 'login': '12345678910'})
-        
-        # Find and deactivate warehouse so there is no warehouse to find
-        warehouse = Warehouse.search([('company_id', '=', test_user.company_id.id)])
-        warehouse.active = False
+        # Make it so warehouse isn't found in search
+        self.test_company_warehouse.active = False
 
         with self.assertRaises(ValidationError) as e:
-            User.sudo(test_user).get_user_warehouse()
+             self.do_as_user(self.test_user).get_user_warehouse()
 
         self.assertEqual(e.exception.name, 'Cannot find a warehouse for user')
 
-    def test_03_get_user_warehouse_multiple_warehouses(self):
+    def test03_get_user_warehouse_multiple_warehouses(self):
         """Checking that when a user has mutiple warehouses 
            the correct error is raised
         """
         Settings = self.env['res.config.settings']
-        Warehouse = self.env['stock.warehouse']
-        User = self.env['res.users']
-
-        
         test_settings = Settings.create({'group_stock_multi_warehouses': True,
                                          'group_stock_multi_locations': True})
         test_settings.execute()
 
-
-        # Creating user without company so takes company from current user
-        test_user = User.create({'name': 'test_user',
-                                 'login': '12345678910'})
-        
-        # Find and deactivate warehouse so there is no warehouse to find
-        warehouse1 = Warehouse.search([('company_id', '=', test_user.company_id.id)])
-        warehouse2 = warehouse1.copy({'name': '123',
+        # Create new warehouse by copying current one and changing
+        # the required feilds
+        warehouse2 = self.test_company_warehouse.copy({'name': '123',
                                       'code': '123'})
 
         with self.assertRaises(ValidationError) as e:
-            User.sudo(test_user).get_user_warehouse()
+            self.do_as_user(self.test_user).get_user_warehouse()
 
         self.assertEqual(e.exception.name, 'Found multiple warehouses for user')
 
-    def test_04_get_user_warehouse_success(self):
+    def test04_get_user_warehouse_success(self):
         """Checks that the correct warehouse is returned
         """
-        Company = self.env['res.company']
-        Warehouse = self.env['stock.warehouse']
-        User = self.env['res.users']
-        
-        test_company = Company.create({'name': 'test_company'})
-        company_warehouse = Warehouse.search([('company_id', '=', test_company.id)])
-
-        # Creating user without company so takes company from current user
-        test_user = User.create({'name': 'test_user',
-                                 'login': '12345678910',
-                                 'company_id': test_company.id,
-                                 'company_ids': [(6, 0, test_company.ids)]})
-
-        returned_warehouse = User.sudo(test_user).get_user_warehouse()
-        self.assertEqual(returned_warehouse.id, company_warehouse.id)
+        # Create new company so there is more than one warehouse
+        returned_warehouse = self.do_as_user(self.test_user).get_user_warehouse()
+        self.assertEqual(returned_warehouse.id, self.test_company_warehouse.id)
